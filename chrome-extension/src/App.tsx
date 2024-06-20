ee8e9502f828632544733a5388855a4349909599
rudaks <rudaks94@gmail.com>
2024-06-20 20:22:24 +0900

    update

---
+++ chrome-extension/src/App.tsx
@@ -0,0 +1,17 @@
+/// <reference types="chrome" />
+/// <reference types="vite-plugin-svgr/client" />
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello, World!</p>
+        <p>I'm a Chrome Extension Popup!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
